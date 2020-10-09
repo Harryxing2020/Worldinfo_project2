c3fd3c8ac59d5e48db4a104817764f60d1ce9c03
harryxing@hotmail.com <harryxing@hotmail.com>
2020-10-08 18:18:58 -0700

    the 2nd commit

---
+++ static/js/showChartNew.js
@@ -0,0 +1,119 @@
+// svg container
+var height = 600;
+var width = 1000;
+
+// margins
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+// chart area minus margins
+var chartHeight = height - margin.top - margin.bottom;
+var chartWidth = width - margin.left - margin.right;
+
+function buildCharts(country) {
+
+
+    d3.json("/getCountryData")
+        .then(function (countrys) {
+
+            console.log(countrys);
+            // data
+            var dataArray = countrys.map(country => country.happiestScore)
+            var dataCategories = countrys.map(country => country.country)
+
+            console.log(dataArray)
+            console.log(dataCategories)
+
+            // create svg container
+            var svg = d3.select("body").append("svg")
+                .attr("height", height)
+                .attr("width", width);
+
+            // shift everything over by the margins
+            var chartGroup = svg.append("g")
+                .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+            // scale y to chart height
+            var yScale = d3.scaleLinear()
+                .domain([0, d3.max(dataArray)])
+                .range([chartHeight, 0]);
+
+            // scale x to chart width
+            var xScale = d3.scaleBand()
+                .domain(dataCategories)
+                .range([0, chartWidth])
+                .padding(0.1);
+
+            // create axes
+            var yAxis = d3.axisLeft(yScale);
+            var xAxis = d3.axisBottom(xScale);
+
+            // set x to the bottom of the chart
+            chartGroup.append("g")
+                .attr("transform", `translate(0, ${chartHeight})`)
+                .call(xAxis);
+
+            // set y to the y axis
+            chartGroup.append("g")
+                .call(yAxis);
+
+            // Create the rectangles using data binding
+            var barsGroup = chartGroup.selectAll("rect")
+                .data(dataArray)
+                .enter()
+                .append("rect")
+                .attr("x", (d, i) => xScale(dataCategories[i]))
+                .attr("y", d => yScale(d))
+                .attr("width", xScale.bandwidth())
+                .attr("height", d => chartHeight - yScale(d))
+                .attr("fill", "green");
+
+            // Create the event listeners with transitions
+            barsGroup.on("mouseover", function () {
+                d3.select(this)
+                    .transition()
+                    .duration(500)
+                    .attr("fill", "red");
+            })
+                .on("mouseout", function () {
+                    d3.select(this)
+                        .transition()
+                        .duration(500)
+                        .attr("fill", "green");
+                });
+        })
+
+
+}
+
+function init() {
+
+    // Set up the dropdown menu
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+    // Use the list of sample names to populate the select options
+
+    d3.json("/getCountryData")
+        .then(function (countrys) {
+            countrys.forEach((country) => {
+                selector
+                    .append("option")
+                    .text(country.country)
+                    .property("value", country.country);
+            });
+            buildCharts(countrys[0].country);
+        });
+
+
+}
+
+function optionChanged(country) {
+    // Fetch new data each time a new state is selected
+    buildCharts(country);
+}
+
+init();
